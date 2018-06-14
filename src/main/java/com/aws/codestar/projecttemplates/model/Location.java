45494c4aa83fd58364295173c1481ff5615c0037
Noel Gonzalez <noel.gonzalez.h@gmail.com>
2018-06-13 19:11:53 -0500

    Adding languages

--- src/main/java/com/aws/codestar/projecttemplates/model/Location.java
+++ src/main/java/com/aws/codestar/projecttemplates/model/Location.java
@@ -1,13 +1,26 @@
 package com.aws.codestar.projecttemplates.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.io.Serializable;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Location implements Serializable{
 
+    @JsonProperty("language_id")
     private Integer language_id;
+    @JsonProperty("name")
     private String name;
+    @JsonProperty("iso_639_1")
     private String iso_639_1;
+    @JsonProperty("locale_language")
     private String locale_language;
+
+    public Location() {
+    }
 
     public Location(Integer language_id, String name, String iso_639_1, String locale_language) {
         this.language_id = language_id;
